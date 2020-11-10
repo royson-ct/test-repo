50b75bca5c91eee68167404831db36f866a79b6d
Royson <royson@clevertap.com>
2020-11-10 11:56:43 +0530

    pointing to test royson app

--- a-normal.js
+++ a-normal.js
@@ -1,7 +1,7 @@
 function __wizrocket() {
 
 
-  var targetDomain = 'sk1-staging-8.wzrkt.com';
+  var targetDomain = 'wzrkt.com';
   // var targetDomain = 'localhost:3838'; //ALWAYS comment this line before deploying
 
   var wz_pr = "https:";
